96ab684e777e88a6aabd909febea0ec340eba7ca
valerianEhret <vkadirovv@gmail.com>
2021-02-09 23:40:56 +0100

    added plugins2

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
     entry: './index.js',
     output: {
-        filename: "bundle.js",
+        filename: "bundle.[hash].js",
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
 }
